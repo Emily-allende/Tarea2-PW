de049f3fd9321d8e0ebded4907cf002d71e20e05
Emily-allende <79849579+Emily-allende@users.noreply.github.com>
2022-05-22 17:55:06 -0300

    Update Tarea2.java

--- Tarea2/src/Tarea2.java
+++ Tarea2/src/Tarea2.java
@@ -1,1 +1,29 @@
+public class Tarea2 {
+	public static void main(String[] args) {
+		Estacionamiento estacionamiento = new Estacionamiento();
+		estacionamiento.inicializar(2, 60, 180); 
+			
+		Persona juan = new Persona("Juan");
+		Auto autoJuan = new Auto(Auto.marca, "BCF-235", juan);
 
+		Estacionamiento.entra(autoJuan); 
+		// se inicializa la cuenta corriente a Juan y se le agrega $60
+
+		Persona ana = new Persona("Ana");
+		Auto autoAna = new Auto(Auto.marca, "BRF-535", ana);
+		Estacionamiento.entra(autoAna);
+
+		Estacionamiento.sale(autoJuan);
+
+		Estacionamiento.nuevoDia(0);  
+		// cierra el estacionamiento y abre el dia siguiente
+
+		Estacionamiento.entra(autoJuan);
+		Persona felipe = new Persona("Felipe");
+		Auto autoFelipe = new Auto(Auto.marca, "BFD-124", felipe);
+		Estacionamiento.entra(autoFelipe);
+		// notar que no entra el auto, asi que no tiene ningun efecto
+
+		Estacionamiento.calcular(0);
+	}
+}
